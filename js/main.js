a2cb12d885fa125557093fa84f4fe944997949ad
Emilie <emilie.chnvt@gmail.com>
2024-12-12 21:12:07 +0100

    token + messages logged

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let apiRegister = "https://b1messenger.esdlyon.dev/login"
+let apiMessage = "https://b1messenger.esdlyon.dev/api/messages"
 
+let token = null
 
-
-let params={
+async function getToken() {
+    let params={
     method: "POST",
     headers:{
         "Content-Type": "application/json"
@@ -12,8 +14,36 @@ let params={
         password: "ghjklmwx",
     })
 }
-fetch(apiRegister, params)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.token)
-    })
+    await fetch(apiRegister, params)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.token)
+            token = data.token
+
+        })}
+
+
+
+
+async function logMessage() {
+    let parameters={
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+
+    }
+
+    await fetch(apiMessage, parameters)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+(async () => {
+    await getToken(); // Récupérer d'abord le token
+    await logMessage(); // Puis récupérer les messages
+})();
+
