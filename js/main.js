ee84416d60e09adbdb33a20c35e65952ae20cfa0
Emilie <emilie.chnvt@gmail.com>
2024-12-15 18:02:26 +0100

    getMessages.then added

--- js/main.js
+++ js/main.js
@@ -1,49 +1,70 @@
-let apiRegister = "https://b1messenger.esdlyon.dev/login"
-let apiMessage = "https://b1messenger.esdlyon.dev/api/messages"
+const api = "https://b1messenger.esdlyon.dev/login";
+const apiMessage = "https://b1messenger.esdlyon.dev/api/messages";
 
-let token = null
+const login = document.querySelector(".login");
+const chat = document.querySelector(".chat");
 
-async function getToken() {
-    let params={
-    method: "POST",
-    headers:{
-        "Content-Type": "application/json"
-    },
-    body:JSON.stringify({
-        username: "emiliech",
-        password: "ghjklmwx",
-    })
-}
-    await fetch(apiRegister, params)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.token)
-            token = data.token
+let token = null;
 
-        })}
-
-
-
-
-async function logMessage() {
-    let parameters={
-        method: "GET",
-        headers:{
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
+async function getToken(username, password) {
+    let params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-
+        body: JSON.stringify({
+            username:username,
+            password:password,
+        })
     }
-
-    await fetch(apiMessage, parameters)
-        .then(response => response.json())
+    return await fetch(api, params)
+        .then(res => res.json())
         .then(data => {
             console.log(data)
+            token = data.token;
+            return data
         })
 }
-async function initMessaagerie(){
-    await getToken();
-    await logMessage();
+
+
+async function getMessages() {
+    let parameters = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+
+    }
+    return await fetch(apiMessage, parameters)
+        .then(res => res.json())
+        .then(data => {
+            return data
+        })
 }
 
-initMessaagerie()
+
+function displayLoginForm(){
+    login.style.display = "flex";
+    const username = document.querySelector(".username");
+    const password = document.querySelector(".password");
+    const submit = document.querySelector(".submit");
+    submit.addEventListener('click', ()=>{
+        getToken(username.value, password.value).then((res)=>{
+            if(res.token){
+
+                displayChat()
+            }
+        })
+    })
+}
+function displayChat(){
+    login.style.display = "none";
+    chat.style.display = "flex";
+    getMessages().then((res)=>{console.log(res)})
+}
+if(!token){
+    displayLoginForm()
+}else{
+    displayChat()
+}
