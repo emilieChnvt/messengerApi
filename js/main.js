e0e426df6aaf59855a14556b512a3f0184fa658c
Emilie <emilie.chnvt@gmail.com>
2024-12-29 18:02:42 +0100

    penToEdit() added

--- js/main.js
+++ js/main.js
@@ -296,6 +296,7 @@ const displayInputMessage = () => {
                         author:{ username:'emiliech'},
                         id: newMessage.id,
                     }, 'group')
+                    toggleMessage()
                 })
 
             })
@@ -308,9 +309,9 @@ const displayInputMessage = () => {
 const addMessageToChat = (message, type) => {
     let allMessagesContainer;
 
-    if(type === 'group'){
+    if (type === 'group') {
         allMessagesContainer = document.querySelector(".allMessages");
-    }else if (type === 'private'){
+    } else if (type === 'private') {
         allMessagesContainer = document.querySelector(".allMessagesPrives");
     }
 
@@ -321,31 +322,52 @@ const addMessageToChat = (message, type) => {
     const messageContent = document.createElement("span");
     messageContent.textContent = message.content;
 
-    if(message.id){
+    if (message.id) {
         messagesDiv.setAttribute("data-message-id", message.id);
     }
     console.log(message)
-    if(message.author.username === "emiliech"){
+    if (message.author.username === "emiliech") {
         const trashElement = deleteElement(messagesDiv, message.id)
         messagesDiv.appendChild(trashElement);
     }
-    if(message.author.username === "emiliech"){
+    if (message.author.username === "emiliech") {
         messagesDiv.style.justifyContent = "flex-end"
         messageContent.style.textAlign = "right";
-    }else{
+    } else {
         messageContent.style.textAlign = "left";
         messagesDiv.style.justifyContent = "flex-start"
     }
 
     let reactions = reactionDiv(message)
+    let pen = penToEdit(message)
 
     messagesDiv.appendChild(messageContent);
     messagesDiv.appendChild(reactions);
+    messagesDiv.appendChild(pen);
 
     allMessagesContainer.appendChild(messagesDiv);
 
     //faire défiler pr voir new message
     allMessagesContainer.scrollTop = allMessagesContainer.scrollHeight;
+
+}
+const penToEdit=()=>{
+    const pencil = document.createElement("span")
+    pencil.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width:15px; height:15px"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>`
+    pencil.addEventListener("click", ()=>{
+        editMessage()
+    })
+    return pencil
+}
+const editMessage = (messageDiv, messageId) => {
+    const inputMessage =document.querySelector(".input");
+    const newContent = inputMessage.value;
+    if(newContent){
+        edit(messageId, newContent).then((res)=> {
+            console.log(res)
+
+        })
+    }
 
 }
 const reactionDiv = (message)=> {
@@ -407,6 +429,7 @@ const deleteElement=(messagesDiv, messageId)=>{
     })
     return trash
 }
+
 
 if(!token){
     displayLoginForm()
@@ -581,7 +604,7 @@ async function emojiReaction(messageId, reactionType){
     })
 }
 
-async function editMessage(messageId){
+async function edit(messageId, inputMessage){
     let params ={
         method: "PUT",
         headers: {
