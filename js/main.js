92df27b48b975c5fb2389fd12f288484830fce41
Emilie <emilie.chnvt@gmail.com>
2025-01-01 19:14:56 +0100

    display response added but not fonctionnelle

--- js/main.js
+++ js/main.js
@@ -306,9 +306,6 @@ const displayMessages = () => {
     getMessages().then(res => {
         chat.classList.remove("hidden");
         chat.classList.add("visible");
-
-        console.log(res)
-
         res.forEach(msg => {
             addMessageToChat(msg, 'group')
         })
@@ -402,9 +399,16 @@ console.log(message)
         authorOfMessage.textContent = message.author.username ;
 
         const  reactions = reactionDiv(message)
-        contentContainer.appendChild(reactions)
+        contentContainer.appendChild(reactions);
+
+        const responseButton = document.createElement("button");
+        responseButton.textContent = "Répondre";
+        responseButton.classList.add("responseButton");
+
+        responseButton.addEventListener("click", ()=>toggleMessageToAddResponse(message));
 
         messagesDiv.appendChild(authorOfMessage)
+        messagesDiv.appendChild(responseButton)
 
     }
 
@@ -420,6 +424,52 @@ console.log(message)
     allMessagesContainer.scrollTop = allMessagesContainer.scrollHeight;
 
 }
+const toggleMessageToAddResponse = (message) => {
+    const responseDiv = document.createElement("div");
+    responseDiv.classList.add("responseDiv");
+    responseDiv.setAttribute("data-message-id", message.id);
+
+    const responseInput = document.createElement("textarea");
+    responseInput.classList.add("responseInput");
+
+    const sendResponse=document.createElement("button");
+    sendResponse.classList.add("sendResponse");
+    sendResponse.textContent = "envoyer";
+
+    sendResponse.addEventListener("click", ()=>{
+        response(message).then((res)=>{
+            console.log(res);
+            displayResponse()
+        })
+
+    })
+    responseDiv.appendChild(responseInput);
+    responseDiv.appendChild(sendResponse);
+
+    const messageDiv = document.querySelector(`.messagesDiv[data-message-id="${message.id}"]`);
+    messageDiv.appendChild(responseDiv);
+
+}
+const displayResponse=(messageId, replyData)=>{
+    const messageDiv = document.querySelector(`.messagesDiv[data-message-id="${message.id}"]`);
+
+    const replyDiv = document.createElement("div");
+    replyDiv.classList.add("replyDiv");
+
+    const author = document.createElement("span");
+    author.classList.add("authorDiv");
+    author.innerHTML = `${replyData.author.username}`;
+    const responseContent = document.createElement("span");
+    responseContent.classList.add("responseContent");
+    responseContent.textContent = replyData.content ;
+
+    replyDiv.appendChild(author);
+    replyDiv.appendChild(responseContent);
+
+    messageDiv.appendChild(replyDiv);
+
+}
+
 const penToEdit=(message)=>{
     const pencil = document.createElement("span")
     pencil.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width:20px; height:20px; padding:5px; background-color:#f0f0f0; border-radius:50%; cursor:pointer; position: absolute; left:-5px; bottom:50%"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>`
@@ -748,7 +798,7 @@ async function updateMessage(messageId, newContent){
         return data
     })
 }
-async function response(itemId){
+async function response(itemId, content){
     let params ={
         method: "POST",
         headers: {
@@ -756,7 +806,7 @@ async function response(itemId){
             "Authorization": "Bearer " + token
         },
         body: JSON.stringify({
-            content: inputMessage
+            content
         })
     }
     return await fetch(`https://b1messenger.esdlyon.dev/api/responses/${itemId}/new`, params)
