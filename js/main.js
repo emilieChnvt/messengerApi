f628c6a2259f5cb25fd56443a85dc50c703f10be
Emilie <emilie.chnvt@gmail.com>
2025-01-07 21:34:37 +0100

    css

--- js/main.js
+++ js/main.js
@@ -126,15 +126,11 @@ const chooseBtnConv = ()=> {
 }
 
 const displayChatConvPrive = ()=>{
-
     const allConv = document.querySelector(".allConvPrives");
     allConv.innerHTML = "";
-
     convsPrive().then((res)=>{
         conversations = res
-
         res.forEach((item)=>{
-
             let conv = `<div class="grpPrive d-flex align-items-center justify-content-between borderOrange rounded-3 px-3 py-1" id="${item.id}">
                                     <div class=" d-flex align-items-center justify-content-center">
                                         <div class="nameGroupe rounded-circle photoProfil border border-1 me-3"></div>
@@ -145,8 +141,6 @@ const displayChatConvPrive = ()=>{
                                </div>`
 
             allConv.innerHTML += conv
-
-
         })
         toogleBtnToShowConvPrive()
     })
@@ -488,17 +482,14 @@ const displayMessages = () => {
     displayInputMessage()
 }
 
-const displayInputMessage = () => {
+const displayInputMessage = (file) => {
 
     const btnMessage = document.querySelector(".btnMessage");
     btnMessage.addEventListener("click", ()=>{
         const message = inputMessage.value;
         if(message){
             newMessage(message).then((res)=> {
-                console.log('new',res);
-                //pour récupérer les messages et les id
                 getMessages().then(res => {
-                    // Le dernier message ajouté est généralement le plus récent
                     const newMessage = res[res.length - 1];
                     addMessageToChat({
                         content:message,
@@ -509,6 +500,8 @@ const displayInputMessage = () => {
 
             })
             inputMessage.value = "";
+        }else if(message && file){
+            addImageToChat()
         }
 
 
@@ -587,7 +580,7 @@ const addProfileImg=(message)=>{
     photoProfil.src = imageUrl;
     return photoProfil;
 }
-const authorAction = (message, messagesDiv, )=>{
+const authorAction = (message, messagesDiv, type )=>{
     if (message.author.username === currentUser) {
         console.log(currentUser)
         messagesDiv.classList.add("marg");
@@ -596,9 +589,6 @@ const authorAction = (message, messagesDiv, )=>{
 
         const trashElement = deleteElement(messagesDiv, message.id)
         messagesDiv.appendChild(trashElement);
-
-        const pen = penToEdit(message)
-        messagesDiv.prepend(pen);
     } else {
         const authorOfMessage = document.createElement("span");
         authorOfMessage.classList.add("authorOfMessage");
@@ -606,7 +596,10 @@ const authorAction = (message, messagesDiv, )=>{
         messagesDiv.appendChild(authorOfMessage);
         messagesDiv.style.justifyContent = "flex-start"
 
-
+    }
+    if(message.author.username === currentUser && type === 'group'){
+        const pen = penToEdit(message)
+        messagesDiv.prepend(pen);
     }
 }
 const createResponseButton=(message)=>{
@@ -719,8 +712,6 @@ const penToEdit=(message)=>{
     const pencil = document.createElement("span")
     pencil.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width:20px; height:20px; padding:5px; background-color:#f0f0f0; border-radius:50%; cursor:pointer; position: absolute; left:-5px; bottom:50%"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>`
     pencil.addEventListener("click", ()=>{
-        console.log("messageId avant l'appel:", message.id);
-
         editMessage(message, message.id)
     })
     return pencil
@@ -793,8 +784,13 @@ fileInput.addEventListener("change", (e) => {
     if(file){
         const res = privatePhoto(file)
         if(res.imageId){
-            addImageToChat(res.imageUrl)
-            console.log('image with id', res.imageId)
+            //recup id
+            const inputMessagePrive = document.querySelector(".inputMessagePrive");
+            const messageResponse = sendMessageWithImage(res.imageId, inputMessagePrive);
+            if(messageResponse){
+                addImageToChat(imageUrl)
+            }
+
         }
     }
 })
